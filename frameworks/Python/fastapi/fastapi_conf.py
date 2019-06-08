27ade6d719b51bae8158f4e4b2ce27bff12c7816
euri10 <benoit.barthelet@gmail.com>
2019-06-08 09:42:51 +0200

    Upgraded FastAPI and related libs Tweaked gunicorn conf file for pypy using different workers once there's some async pypy db libs Added orm version of app, WIP

--- frameworks/Python/fastapi/fastapi_conf.py
+++ frameworks/Python/fastapi/fastapi_conf.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import os
+import sys
 
+_is_pypy = hasattr(sys, 'pypy_version_info')
 _is_travis = os.environ.get('TRAVIS') == 'true'
 
 workers = multiprocessing.cpu_count()
@@ -12,3 +14,8 @@ keepalive = 120
 errorlog = '-'
 pidfile = '/tmp/fastapi.pid'
 loglevel = 'error'
+
+if _is_pypy:
+    worker_class = "uvicorn.workers.UvicornH11Worker"
+else:
+    worker_class = "uvicorn.workers.UvicornWorker"
